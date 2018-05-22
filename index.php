5ab5ed57bbc4989840bcf1bddf7cd9b08225cb3e
esteban1234 <tva_libra@hotmail.com>
2018-05-22 18:05:05 -0500

    INICIO DE PAGINA

---
+++ index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    
+    <link rel="icon" type="image/png" href="img/favicon.png" />
+    <title>SERVICIOS CORPORATIVOS CO Y P </title>
+   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
+    <META NAME="Title" content="SERVICIOS CORPORATIVOS CO Y P SA DE CV">
+    <META NAME="Author" CONTENT="[]"/>
+    <META name="Description" content="Somos una compañía dedicados a ofrecer construcción de casas, remodelación, contrucción de carreteras, edificios y ampliaciones."/>
+    <META NAME="Keywords" CONTENT="construcción , construcción COYP , COYP , contrucción de casas , ampliaciones de casa , ingenieria civil , productos de construcción, levantamientos, desarrollo de proyectos de construcción"/>
+    <!-- <link rel="stylesheet" href="css/iconos.min.css"> -->
+    <link rel="stylesheet" href="css/fontawesome-all.css">
+    <!-- <link rel="stylesheet" href="css/fontawesome-all.min.css"> -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <!-- <link rel="stylesheet" href="css/responsivo.css"> -->
+    <link href="https://fonts.googleapis.com/css?family=Khula|Montserrat|Roboto" rel="stylesheet">
+    <script src="js/fontawesome-all.js"></script>
+
+
+
+    <script src="js/smooth-scroll.min.js"></script>
+    <script>
+      smoothScroll.init({
+      selector: '[data-scroll]',
+      selectorHeader: null,
+      speed: 2000,
+      easing: 'easeInOutCubic',
+      offset: 0,
+      callback: function ( anchor, toggle ) {}
+      });
+    </script>
+
+  </head>
+  <body>
+
+    
+
+    <div class="banner"  id="INICIO">
+      <div class="fondo">
+        <div class="menu">
+          <nav class="pull-left">
+            <ul>
+              <a class="selector" id="select" data-scroll href="#INICIO" aria-pressed="true">INICIO</a>
+              <a class="selector" data-scroll href="#NOSOTROS" aria-pressed="true">NOSOTROS</a>
+              <a class="selector" data-scroll href="#SERVICIOS" aria-pressed="true">SERVICIOS</a>
+              <a class="selector" data-scroll href="#CONTACTO" aria-pressed="true">CONTACTO</a>
+            </ul>
+          </nav>
+        
+          <figure class="logo pull-right">
+            <img src="img/coyp.png" alt="CO Y P" class="img-responsive">
+          </figure>
+         
+        </div>
+
+        <div class="tit">Arquitectura & Construcción</div>
+        <center style="margin-top: 3%;">
+            <a data-scroll href="#CONTACTO" aria-pressed="true"><button class="btn btn-lg btn-secondary">CONTACTANOS</button></a> 
+        </center>
+      </div>
+    </div>
+
+
+<section class="seccion2" id="NOSOTROS" style="padding: 5% 0% 0% 0%;">
+
+  <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="iconos">
+              <span class="fa-stack fa-lg fa-5x" style="color: #B579D2;">
+                <i class="fa fa-circle fa-stack-2x"></i>
+                <i class="fas fa-building fa-stack-1x fa-inverse" style="color: #fff"></i>
+              </span>
+            </div>
+            <h2 class="text-center">Construción</h2>
+            <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p>
+          </div>
+          <div class="col-md-4">
+            <div class="iconos">
+              <span class="fa-stack fa-lg fa-5x" style="color: #B579D2;">
+                <i class="fa fa-circle fa-stack-2x"></i>
+                <i class="fab fa-codepen fa-stack-1x fa-inverse" style="color: #fff"></i>
+              </span>
+            </div>
+            <h2 class="text-center">Arquitectura</h2>
+            <p class="text-center">Contamos con arquitectos especializados en planos arquitectónicos, para que su desarrollo sea de total calidad y gusto del cliente antes durante y despues de cada proyecto.</p>
+          </div>
+          <div class="col-md-4">
+            <div class="iconos">
+              <span class="fa-stack fa-lg fa-5x" style="color: #B579D2;">
+                <i class="fa fa-circle fa-stack-2x"></i>
+                <i class="fas fa-desktop fa-stack-1x fa-inverse" style="color: #fff"></i>
+              </span>
+            </div>
+            <h2 class="text-center">Diseño</h2>
+            <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+</section>
+
+<section class="seccion1" id="SERVICIOS" >
+
+ 
+</section>
+
+
+<section class="seccion3" id="CONTACTO" style="padding: 5% 0%;">
+
+</section>
+
+
+<footer>
+<p>Derechos de autor 2018 © |  SERVICIOS CORPORATIVOS CO Y P SA DE CV</p>
+</footer>
+
+
+    <script src="js/sendCORREO.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>
+  </body>
+</html>
