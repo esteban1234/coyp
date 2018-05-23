bcf35a7af6162d98325934275c16115026c011ad
esteban1234 <tva_libra@hotmail.com>
2018-05-23 10:50:52 -0500

    avance de pagina

--- index.php
+++ index.php
@@ -12,11 +12,12 @@
     <META NAME="Keywords" CONTENT="construcción , construcción COYP , COYP , contrucción de casas , ampliaciones de casa , ingenieria civil , productos de construcción, levantamientos, desarrollo de proyectos de construcción"/>
     <!-- <link rel="stylesheet" href="css/iconos.min.css"> -->
     <link rel="stylesheet" href="css/fontawesome-all.css">
-    <!-- <link rel="stylesheet" href="css/fontawesome-all.min.css"> -->
+    <link rel="stylesheet" href="css/fontawesome-all.min.css">
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/estilos.css">
     <!-- <link rel="stylesheet" href="css/responsivo.css"> -->
     <link href="https://fonts.googleapis.com/css?family=Khula|Montserrat|Roboto" rel="stylesheet">
+    
     <script src="js/fontawesome-all.js"></script>
 
 
@@ -64,7 +65,7 @@
     </div>
 
 
-<section class="seccion2" id="NOSOTROS" style="padding: 5% 0% 0% 0%;">
+<section class="seccion2" id="NOSOTROS" style="padding: 5% 0% 5% 0%;">
 
   <div class="section">
       <div class="container">
@@ -76,10 +77,8 @@
                 <i class="fas fa-building fa-stack-1x fa-inverse" style="color: #fff"></i>
               </span>
             </div>
-            <h2 class="text-center">Construción</h2>
-            <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisici elit,
-              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
-              <br>Ut enim ad minim veniam, quis nostrud</p>
+            <h2 class="text-center" style="color: #717d7e;">Construción</h2>
+            <p class="text-center" style="color:   #839192 ;">Construimos de acuerdo a la medida de tu proyecto arquitectónico y en base a tu presupuesto.</p>
           </div>
           <div class="col-md-4">
             <div class="iconos">
@@ -88,8 +87,8 @@
                 <i class="fab fa-codepen fa-stack-1x fa-inverse" style="color: #fff"></i>
               </span>
             </div>
-            <h2 class="text-center">Arquitectura</h2>
-            <p class="text-center">Contamos con arquitectos especializados en planos arquitectónicos, para que su desarrollo sea de total calidad y gusto del cliente antes durante y despues de cada proyecto.</p>
+            <h2 class="text-center" style="color: #717d7e;">Arquitectura</h2>
+            <p class="text-center" style="color:   #839192 ;">Contamos con arquitectos especializados en planos arquitectónicos, para que su desarrollo sea de total calidad y gusto del cliente antes durante y despues de cada proyecto.</p>
           </div>
           <div class="col-md-4">
             <div class="iconos">
@@ -98,20 +97,87 @@
                 <i class="fas fa-desktop fa-stack-1x fa-inverse" style="color: #fff"></i>
               </span>
             </div>
-            <h2 class="text-center">Diseño</h2>
-            <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisici elit,
-              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
-              <br>Ut enim ad minim veniam, quis nostrud</p>
+            <h2 class="text-center" style="color: #717d7e;">Diseño</h2>
+            <p class="text-center" style="color:   #839192 ;">Te ayudamos a crear un proyecto arquitectónico que se adecue a tus necesidades y presupuesto.</p>
           </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="section">
+      <div class="container">
+        <div class="row">
+          <p class="tit2">Conoce mas sobre CO Y P</p>
+          <p class="text">Somos una empresa que proporciona un servicio integral a cualquier persona que desee construir o remodelar, te apoyamos desde el diseño de tu proyecto, realizamos todos los trámites que requieras para construir y edificamos tu proyecto hasta su finalización. <br><br>
+
+          Los servicios que ofrece CO Y P son garantía de eficiencia, honestidad, calidad y profesionalismo.  <br><br>
+
+          Con los mejores materiales de construcción y aceros, proyectamos, diseñamos y construimos obras civiles con tecnología y servicio a nuestros clientes con la más alta calidad, asistiendo con la selección de materiales más acordes con sus necesidades específicas. Además les ayudamos a resolver el mejor uso del espacio disponible con la más alta funcionalidad de sus instalaciones presentes y futuras.</p>
         </div>
       </div>
     </div>
   
 </section>
 
-<section class="seccion1" id="SERVICIOS" >
-
- 
+<section class="seccion1" id="SERVICIOS" style="padding: 5% 0%; background: #f5eef8;" >
+  <section id="what-we-do">
+    <div class="container-fluid">
+      <h2 class="section-title  h1">SERVICIOS</h2>
+      <p class="text-center text-muted h5 text2" >En CO Y P nos preocupamos por cada detalle que hacen del sitio un espacio con uso responsable e incluyente para todos los usuarios y todas las escalas.</p>
+      <div class="row " style="margin-top: 3%;">
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4">
+          <div class="card">
+            <div class="card-block block-1">
+              <h3 class="card-title">Levantamiento arquitectonico y estudios financieros</h3>
+              <p class="card-text">Mediante la contratación de nuestros servicios, le damos una amplia asesoria sobre su proyecto, cubriendo todas la necesidades requeridas.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4">
+          <div class="card">
+            <div class="card-block block-2">
+              <h3 class="card-title">Desarrollo de proyectos y ejecución de obra</h3>
+              <p class="card-text">Nuestros servicios son especializados para cada proyecto asi como el compromiso de iniciarlo y finalizarlo correctamente a los lineamientos planteados.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4">
+          <div class="card">
+            <div class="card-block block-3">
+              <h3 class="card-title">Remodelaciones</h3>
+              <p class="card-text">Nuestros servicios incluyen todo tipo de remodelación a nivel estructural. Ofrecemos soluciones de pintura para interior y exterior de los espacios, fontaneria, electricidad, albañileria, maquinarias, etc. Para edificios y casas.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4">
+          <div class="card">
+            <div class="card-block block-4">
+              <h3 class="card-title">Ampliaciones</h3>
+              <p class="card-text">Podemos hacer ampliaciones de una casa o edificio antiguo y que éstas tengan ambientes totalmente diferentes en cuanto a iluminación, ventilación y diseño de interiores, haz la ampliación diferente al estilo antiguo pero respetando algunos detalles para no romper con la armonía.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4">
+          <div class="card">
+            <div class="card-block block-5">
+              <h3 class="card-title">Lectura e interpretación de planos</h3>
+              <p class="card-text">Cuentas con el plano de tu casa o edificio y quieres remodelar? En PetrusRomane analizamos y complementamos los proyectos arquitectónicos y de ingeniería mediante la elección de los más eficientes sistemas constructivos y de remodelación.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4">
+          <div class="card">
+            <div class="card-block block-6">
+              <h3 class="card-title">Diseño arquitectonico</h3>
+              <p class="card-text">Lo mas importante para nosotros es hacer realidad las espectativas de nuestros clientes en cualquier proyecto. Ofrecemos servicios de diseño y construcción a partir de ideas iniciales para la construcción final.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>  
+  </section>
 </section>
 
 
