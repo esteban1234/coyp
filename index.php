a1c022dcd578f5aaa12d206c9a5c41a1e54ef46f
esteban1234 <tva_libra@hotmail.com>
2018-06-05 13:45:28 -0500

    metas

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="es">
   <head>
     <meta charset="utf-8">
-
+    <meta name="google-site-verification" content="W2whTb6ag92jyqdHssRQj3bMBhm393SjO66viOxIs5Q" />
     <link rel="icon" type="image/png" href="img/favicon.png" />
     <title>SERVICIOS CORPORATIVOS CO Y P </title>
    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
